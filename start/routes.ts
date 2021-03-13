db788c7f8e9ae67d9c3b804736d6f43bcbad5d35
Cdric-art <cdric.art@gmail.com>
2021-03-13 14:04:55 +0100

    Feat: AuthController

--- start/routes.ts
+++ start/routes.ts
@@ -24,9 +24,18 @@ Route.get('/api', async () => {
   return { hello: 'world' }
 })
 
-// User
+// Users
+Route.get('/api/admin', 'AuthController.me').middleware('auth')
+Route.post('/api/admin/register', 'AuthController.register')
+Route.post('/api/admin/login', 'AuthController.login')
+
 
 // CRUD Post
 Route
   .resource('/api/posts', 'PostsController')
+  .apiOnly()
+
+// CRUD Comments
+Route
+  .resource('/api/comments', 'CommentsController')
   .apiOnly()
