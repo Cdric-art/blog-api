8bbcac94029bd50080c6e61ba2ed6646868b8ff5
Cdric-art <cdric.art@gmail.com>
2021-03-13 18:37:42 +0100

    Feat: Comments validator

---
+++ app/Validators/CommentValidator.ts
@@ -0,0 +1,18 @@
+import { schema } from '@ioc:Adonis/Core/Validator'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CommentValidator {
+  constructor (protected ctx: HttpContextContract) {
+  }
+
+  public schema = schema.create({
+    pseudo: schema.string(),
+    content: schema.string(),
+    post_id: schema.number()
+  })
+
+
+  public messages = {
+    'pseudo': 'Pseudo obligatoire'
+  }
+}
