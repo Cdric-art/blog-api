c45f1f55e5f046fa273d3de54d677ea734a23f04
Cdric-art <cdric.art@gmail.com>
2021-03-27 12:08:18 +0100

    Styles

--- client/pages/components/UpdatedPostForm.js
+++ client/pages/components/UpdatedPostForm.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import styles from "../../styles/Home.module.css";
 import { useState } from "react";
 
-export default function UpdatedPostForm ({ me, title, content, error, success, post_id }) {
+export default function UpdatedPostForm({me, title, content, error, success, post_id}) {
 
     const [t, setT] = useState(title)
     const [c, setC] = useState(content)
@@ -44,15 +44,13 @@ export default function UpdatedPostForm ({ me, title, content, error, success, p
     return <form onSubmit={handleSubmit}>
         <div className={styles.formGroup}>
             <input type="text" name="title" value={t} onChange={(e) => setT(e.target.value)}/>
-        </div>
-        <div className={styles.formGroup}>
             <textarea name="content" cols="30" rows="10" value={c} onChange={(e) => setC(e.target.value)}/>
         </div>
         <div className={styles.formGroup}>
-            <input type="file" name="img" />
+            <input type="file" name="img"/>
+            <input type="hidden" name="user_id" value={me}/>
+            <button className={styles.submit} type="submit">Valider</button>
+            <button className={styles.delete} type="button" onClick={handleDelete}>Delete</button>
         </div>
-        <input type="hidden" name="user_id" value={me}/>
-        <button type="submit">Valider</button>
-        <button type="button" onClick={handleDelete}>Delete</button>
     </form>
 }
